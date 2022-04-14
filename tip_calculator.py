26aa3c68b414604613453fc7fb625b1d0b200c48
syfulsharif <sharif.syful@gmail.com>
2022-04-14 20:34:46 +0600

    Indentation Added

--- tip_calculator.py
+++ tip_calculator.py
@@ -7,10 +7,15 @@
 
 # Write your code below this line 👇
 print("Welcome to the tip calculator!")
+
 bill = float(input("What was the total bill? "))
+
 # print(type(bill))
 tip_percantage = int(
     input("How much tip would you like to give? 10, 12, or 15? "))
+
 num_people = int(input("How many people to split the bill? "))
+
 bill_per_person = (bill + (bill * (tip_percantage / 100))) / num_people
+
 print(f"Each person should pay: {round(bill_per_person, 2)} ")
