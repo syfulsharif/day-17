4f522944bfc8f6ed49a3b263a646cf977482a9b3
syfulsharif <sharif.syful@gmail.com>
2022-05-14 00:15:06 +0600

    Finished update being pushed

--- bmi2_0.py
+++ bmi2_0.py
@@ -10,8 +10,11 @@ bmi = weight / (height ** 2)
 # BMI Logics
 if bmi < 18.05:
     print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi > 18.5 and bmi > 25:
+elif bmi < 25:
     print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi > 25 and bmi < 30:
+elif bmi < 30:
     print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif bmi > 30 and bmi <
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
