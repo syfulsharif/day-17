986cded28de9043e5190e9db1c56538230a90a5d
syfulsharif <sharif.syful@gmail.com>
2022-05-28 10:10:27 +0600

    Code Added

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,13 @@
 # 🚨 Don't change the code below 👇
+from queue import PriorityQueue
+
+
 year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
+# print(year)
+if (year % 4 == 0 and year % 100 and year % 400):
+    print("Leap year.")
+else:
+    print("Not leap year.")
