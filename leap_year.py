dac2cb4bff834c50f52603d90d83c838ef565f2b
syfulsharif <sharif.syful@gmail.com>
2022-06-01 00:36:12 +0600

    Logic Updated

--- leap_year.py
+++ leap_year.py
@@ -7,7 +7,22 @@ year = int(input("Which year do you want to check? "))
 
 # Write your code below this line 👇
 # print(year)
-if (year % 4 == 0 and year % 100 and year % 400):
-    print("Leap year.")
+# My Solution logic is not correct
+# if (year % 4 == 0 and year % 100 and year % 400):
+#     print("Leap year.")
+# else:
+#     print("Not leap year.")
+
+
+# Correct Solution
+
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
 else:
     print("Not leap year.")
