6752db7f77d4feff307b824c54cd405b80c6776b
syfulsharif <sharif.syful@gmail.com>
2022-08-18 00:23:55 +0600

    Challenge 3 Completed

--- hangman.py
+++ hangman.py
@@ -23,32 +23,30 @@ print(display)
 
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Choose a letter: \n")
-guess = guess.lower()
-print(guess)
 
 # Challenge #2
 # TODO-2: - Loop through each position in the chosen_word;
 # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 # e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-i = 0
-while i < len(chosen_word):
-    if chosen_word[i] == guess:
-        display[i] = guess
-    i += 1
-print(display)
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# Challenge #3
+# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
-# Challenge #2
-# TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+end_of_game = False
 
-# i = 0
-# while i < len(chosen_word):
-#     if chosen_word[i] == guess:
-#         print("Right")
-#     else:
-#         print("Wrong")
-#     i += 1
+while not end_of_game:
+    guess = input("Choose a letter: \n")
+    guess = guess.lower()
+    print(guess)
+    i = 0
+    while i < len(chosen_word):
+        if chosen_word[i] == guess:
+            display[i] = guess
+        i += 1
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+
+# print(display)
