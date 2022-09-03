292d0f419d1c042aff1fc6531141ba7b6e6e3a93
syfulsharif <sharif.syful@gmail.com>
2022-09-03 11:24:22 +0600

    Ceaser Cipher Code Created

---
+++ ceaser_cipher.py
@@ -0,0 +1,30 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(txt, shift_number):
+    encrypted_message = ""
+    splitted_text = [*txt]
+    for i in splitted_text:
+        item_position = alphabet.index(i)
+        encrypted_letter = alphabet[item_position + shift_number]
+        encrypted_message += encrypted_letter
+    return encrypted_message
+
+
+def decrypt(txt, shift_number):
+    decrypted_message = ""
+    splitted_text = [*txt]
+    for i in splitted_text:
+        item_position = alphabet.index(i)
+        decrypted_letter = alphabet[item_position - shift_number]
+        decrypted_message += decrypted_letter
+    return decrypted_message
+
+
+print(decrypt(txt=text, shift_number=shift))
