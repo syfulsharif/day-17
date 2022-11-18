dfa36084e535e284c0c790e034532b9fabd22fff
sharif.syful@gmail.com <sharif.syful@gmail.com>
2022-11-18 11:22:26 +0600

    Day 19 Code Added

--- pythonProject/day-18/main.py
+++ pythonProject/day-18/main.py
@@ -1,17 +1,24 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
+tim = Turtle()
+# tim.shape("turtle")
+# tim.color("red")
+
+# for _ in range(4):
+#     tim.forward(10)
+#     tim.penup(10)
+#     # tim.right(90)
 
 
+def draw_shapes(num_sides):
+    for _ in range(num_sides):
+        angle = 360 / num_sides
+        tim.forward(100)
+        tim.right(angle)
 
 
-
-
-
-
-
-
-
+for shape_side_n in range(3, 11):
+    draw_shapes(shape_side_n)
 
 
 screen = Screen()
